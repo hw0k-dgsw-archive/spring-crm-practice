a08eb1c04a10fe9600812e8dfb2ba03040ca6b92
남현욱 <37534735+hw0k@users.noreply.github.com>
2019-04-03 10:03:00 +0900

    Add missed annotation

--- src/main/java/kr/hs/dgsw/springcrmpractice/config/SupportCORS.java
+++ src/main/java/kr/hs/dgsw/springcrmpractice/config/SupportCORS.java
@@ -1,10 +1,12 @@
 package kr.hs.dgsw.springcrmpractice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class SupportCORS {
     @Bean
     public WebMvcConfigurer addCORSSupport() {
